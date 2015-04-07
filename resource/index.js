48827eba0d591e6ef695c45e2174bffc4b6d0bf0
Alexey Krylov <alexey.krylov@saltpepper.ru>
2015-04-07 15:47:40 +0300

    Адаптировал новую галерею

--- resource/index.js
+++ resource/index.js
@@ -2,5 +2,4 @@ $(document).ready(function(){
   CenterActiveGallery = new SwipeGallery({selector: $('.center_active')})
   CenterActiveLoopGallery = new SwipeGallery({selector: $('.center_active_loop'), loop: true})
   gallery3 = new SwipeGallery({selector: $('.gallery3'), loop: true, positionActive: 'center', elementsOnSide:4})
-
 })
